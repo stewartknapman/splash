f49aab445a0f4f10e1f540299a39e1d89c8eedbc
Stewart Knapman <info@stewartknapman.com>
2015-09-06 12:00:19 +1200

    clean up stuff

--- bin/splash.js
+++ bin/splash.js
@@ -1,54 +1,52 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var chalk = require('chalk');
-var u = require('../lib/utils');
-var Splash = require('../lib/app.js');
-var SplashOut = require('../lib/output.js');
+var fs      = require('fs');
+var chalk   = require('chalk');
+var u       = require('../lib/utils');
+var Splash  = require('../lib/app.js');
 
-var argv = require('minimist')(process.argv.slice(2));
-var cwd = process.cwd();
-var output_dir = (argv.o || argv.output || 'build');
+var argv    = require('minimist')(process.argv.slice(2));
+var cwd     = process.cwd();
+var options = {
+  data_dir:     (argv.d || argv.data || 'data'),
+  output_dir:   (argv.o || argv.output || 'build'),
+  include_dir:  (argv.i || argv.includes || 'includes'),
+  template_dir: (argv.t || argv.templates || 'templates')
+};
 
-var splash = new Splash(cwd, argv);
-var splash_out = new SplashOut(cwd, argv);
+var splash  = new Splash(cwd, options);
 
-var build_and_output = function () {
+function build_and_output () {
   u.log(chalk.blue('Building files.'));
   var build = splash.build();
   u.log(chalk.blue('Outputting built files.'));
-  //splash_out.output(build);
+  splash.output(build);
   u.log(chalk.blue('Done.'));
-};
+}
 
 switch (argv._[0]) {
   case 'watch':
-    // watch files for change
-    // on change run splash.build()
-    // output build files to build dir
-    u.log(chalk.blue('Starting splash watch: '), chalk.magenta(cwd));
     // Note: watch can fire mlitple times: http://stackoverflow.com/questions/12978924/fs-watch-fired-twice-when-i-change-the-watched-file
-    //  maybe look at implimenting this later: https://github.com/paulmillr/chokidar
+    // maybe look at implimenting this later: https://github.com/paulmillr/chokidar
+    u.log(chalk.blue('Starting splash watch: '), chalk.magenta(cwd));
     fs.watch(cwd, { recursive: true }, function (event, fname) {
-      // u.log('WATCH: ', event, ' ', fname);
-      // don't build on the build dir
       var build = true;
       if (fname) {
         var dir = fname.split('/')[0];
-        if (dir === output_dir) build = false;
+        if (dir === flags.output_dir) build = false; // don't build on the build dir
       }
       if (build) build_and_output();
     });
     break;
+    
   case 'serve':
     // setup server, run splash.build() and serve output
     // watch files for change
     // on change run splash.build() and reload
     u.log(chalk.red('SPLASH: serve'));
     break;
+    
   default:
-    // build files once
-    // output them to build dir
     u.log(chalk.blue('Starting splash build: '), chalk.magenta(cwd));
     build_and_output();
 }
